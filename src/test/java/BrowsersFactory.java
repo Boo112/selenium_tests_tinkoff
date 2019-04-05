e6d7edcb1e9409fc50b16018b289b8b1a8b0bcac
Ivan Butin <i.butin0@gmail.com>
2019-04-06 02:08:13 +0500

    first commit

---
+++ src/test/java/BrowsersFactory.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+
+
+public enum BrowsersFactory {
+    chrome {
+        public WebDriver create() {
+            updateProperty("chrome");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            return new ChromeDriver(options);
+        }
+    },
+    chrome_invisible {
+        public WebDriver create() {
+            updateProperty("chrome_invisible");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            return new ChromeDriver(options);
+        }
+    },
+    firefox {
+        public WebDriver create() {
+            updateProperty("firefox");
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.addPreference("dom.webnotifications.enabled", false);
+            return new FirefoxDriver(options);
+        }
+    },
+    opera {
+        public WebDriver create() {
+            updateProperty("opera");
+
+            OperaOptions options = new OperaOptions();
+            options.addArguments("--disable-notifications");
+            return new OperaDriver();
+        }
+
+    };
+
+    public WebDriver create() {
+        return null;
+    }
+
+    void updateProperty(String browserName) {
+        System.out.println(String.format("\nЗапускаем %s-browser......", browserName));
+        if (System.getProperty("browser") == null) System.setProperty("browser", browserName);
+    }
+}
